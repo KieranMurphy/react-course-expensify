b78c7620a70468e2d605b58b0c45942f495a7e1b
Kieran Murphy <kieran.murphy@gmail.com>
2018-12-07 16:15:41 -0500

    adding numeral and updating tests

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
-const ExpenseListItem = (props) => (
-    <div>
-        <Link to={`/edit/${props.id}`}>
-            <h3>{props.description}</h3>
-        </Link>
-        <p>{props.amount} - {props.createdAt}</p>
-    </div>
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
+  </div>
 );
 
 export default ExpenseListItem;
